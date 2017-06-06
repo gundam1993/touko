54df1ccd457c147aefbac00ef7f0524bf1769307
gundam1993 <gundam1993@gmail.com>
2017-06-06 13:53:37 +0800

    bug fix

--- modelSync.js
+++ modelSync.js
@@ -1,6 +1,19 @@
 const sequelize = require('./models/database.js')
 const Post = require('./models/post')
+const User = require('./models/user')
+const Introduction = require('./models/Introduction')
+
+User.hasMany(Post, {foreignKey:'user_id', targetKey:'id', as:'Post'})
+User.hasOne(Introduction);
 
 Post.sync().then(function (result) {
+  console.log(result)
+})
+
+User.sync().then(function (result) {
+  console.log(result)
+})
+
+Introduction.sync().then(function (result) {
   console.log(result)
 })
