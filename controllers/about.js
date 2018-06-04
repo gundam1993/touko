f43bd3786e7a6affe948a488e4a0db4444edfb6a
gundam1993 <gundam1993@gmail.com>
2018-06-04 13:59:57 +0800

    [feature]update api

--- controllers/about.js
+++ controllers/about.js
@@ -1,50 +1,20 @@
-const Introduction = require('../models/introduction')
-const Marked = require('marked')
-const marked = Marked.setOptions({
-  renderer: new Marked.Renderer(),
-  gfm: true,
-  tables: true,
-  breaks: false,
-  pedantic: false,
-  sanitize: false,
-  smartLists: true,
-  smartypants: false,
-  langPrefix: 'hljs ',
-  highlight: function (code) {
-    return require('highlight.js').highlightAuto(code).value
-  }
-})
+const marked = require('./helper').marked
 
-exports.getAboutInfo = async (ctx, next) => {
-  try {
-    let about = await Introduction.findOne({attributes: ['content']})
-    if (about) {
-      ctx.response.body = { success: 1, content: about.content, html: marked(about.content) }
-    } else {
-      ctx.response.body = { success: 1, content: '', html: '' }
-    }
-  } catch (e) {
-    ctx.response.body = { success: 1, msg: `获取失败:${e.message}` }
+exports.getAboutInfo = async ({response, model}, next) => {
+  const about = await model.Introduction.findOne({attributes: ['content']})
+  response.body = {
+    success: 1,
+    content: about.content || '',
+    html: about ? marked(about.content) : ''
   }
 }
 
-exports.updateAboutInfo = async (ctx, next) => {
-  try {
-    let userId = ctx.userInfo.userId
-    let content = ctx.request.body.content
-    let about = await Introduction.findOne({ where: { userId: userId } })
-    let info
-    if (about) {
-      info = await Introduction.update({ content: content },
-        { where: { userId: userId } })
-    } else {
-      info = Introduction.create({content: content, userId: userId})
-    }
-    if (!info) {
-      throw new Error()
-    }
-    ctx.response.body = { success: 1, msg: '更新成功' }
-  } catch (e) {
-    ctx.response.body = { success: 1, msg: `更新失败:${e.message}` }
-  }
+exports.updateAboutInfo = async ({response, request, userInfo, model}, next) => {
+  let userId = userInfo.userId
+  const {content} = request.body
+  const about = await model.Introduction.findOne({ where: { userId: userId } })
+  let info = (about
+    ? await model.Introduction.update({ content: content }, { where: { userId: userId } })
+    : await model.Introduction.create({content: content, userId: userId}))
+  response.body = { success: 1, msg: '更新成功' }
 }
