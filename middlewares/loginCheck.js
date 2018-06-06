3e67ff983e2ae048e40c4f50068af48564e08fc2
gundam1993 <gundam1993@gmail.com>
2018-06-06 11:42:05 +0800

    [bug]bug fix

--- middlewares/loginCheck.js
+++ middlewares/loginCheck.js
@@ -1,18 +1,16 @@
 const jwt = require('jsonwebtoken')
-const config = require('config-lite')
 
 var loginCheck = async(ctx, next) => {
   let token = ctx.cookies.get('touko-blog-token') || ctx.request.headers['x-token']
   console.log(token)
   try {
-    let user = jwt.verify(token, config.jwt.key)
+    let user = jwt.verify(token, ctx.app.config.jwt.key)
     ctx.userInfo = user
-     await next()
-  }
-  catch (e) {
+    await next()
+  } catch (e) {
     console.log(e)
     ctx.status = 200
-    ctx.response.body = {success: 0, msg:e}
+    ctx.response.body = {success: 0, msg: e}
   }
 }
 
