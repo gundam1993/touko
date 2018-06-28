0d3271e40771e7caf67076113b0aa72d9a11507e
gundam1993 <gundam1993@gmail.com>
2018-06-28 16:01:51 +0800

    分离管理面板页面开发环境的启动命令

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const Koa = require('koa')
+const path = require('path')
 const config = require('./middlewares/config')
 const router = require('./router')
 const koaBody = require('koa-body')
 const logger = require('koa-logger')
+const staticMiddleware = require('./middlewares/static')
 const sequlize = require('./middlewares/sequelize')
 const errorHandler = require('./middlewares/errorHandler')
-const { Nuxt, Builder } = require('nuxt')
+const { Nuxt, Builder } = require('nuxt-edge')
 // Import and Set Nuxt.js options
 let nuxtConfig = require('./nuxt.config.js')
 nuxtConfig.dev = !(process.env.NODE_ENV === 'production')
+const args = process.argv.splice(2)
 
 async function start () {
   console.log(`Running on ${process.env.NODE_ENV || 'development'} mode`)
@@ -25,29 +28,45 @@ async function start () {
   // error handler
   app.use(errorHandler(app))
   // bodyparser
-  app.use(koaBody({multipart: true}))
+  app.use(koaBody({ multipart: true }))
   // connect database
   sequlize(app, app.config.db)
+  const ADMIN_DIST_DIR = path.join(__dirname, app.config.AdminDir.dist)
+  const STATIC_DIR = path.join(__dirname, app.config.AdminDir.static)
+  app.use(staticMiddleware({
+    dir: [ADMIN_DIST_DIR, STATIC_DIR],
+    prefix: '/admin'
+  }, app))
   const nuxt = new Nuxt(nuxtConfig)
-  // Build in development
-  if (nuxtConfig.dev) {
+  const targetIndex = args.indexOf('--target')
+  const target = args[targetIndex + 1] || ''
+  if (target === 'admin' && !app.isProduction) {
+    console.log('Dev Admin')
+    // 开发模式下使用 koa-webpack 热更新页面
+    app.use(router.routes())
+    const webpackMiddleware = require('./middlewares/webpackMiddleware')
+    webpackMiddleware(app)
+  } else if (!app.isProduction) {
+    // Build in development
     const builder = new Builder(nuxt)
     await builder.build()
-  }
-  // add router middleware:
-  app.use(router.routes())
-  app.use(async (ctx, next) => {
-    await next()
-    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-    return new Promise((resolve, reject) => {
-      ctx.res.on('close', resolve)
-      ctx.res.on('finish', resolve)
-      nuxt.render(ctx.req, ctx.res, promise => {
-        // nuxt.render passes a rejected promise into callback on error.
-        promise.then(resolve).catch(reject)
+    app.use(router.routes())
+  } else {
+    // add router middleware:
+    app.use(router.routes())
+    app.use(async (ctx, next) => {
+      await next()
+      ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+      return new Promise((resolve, reject) => {
+        ctx.res.on('close', resolve)
+        ctx.res.on('finish', resolve)
+        nuxt.render(ctx.req, ctx.res, promise => {
+          // nuxt.render passes a rejected promise into callback on error.
+          promise.then(resolve).catch(reject)
+        })
       })
     })
-  })
+  }
   app.listen(app.config.port, () => console.log(`app listening on port ${app.config.port}!`))
 }
 
