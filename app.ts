1b35cbc016ddfe6dd585f3b83744e3d06afa9010
gundam1993 <gundam1993@gmail.com>
2018-06-29 14:10:29 +0800

    rewrite app.js and server.js with typescript

--- app.ts
+++ app.ts
@@ -1,3 +1,15 @@
-var app = require('./server')
+import ModifiedKoa from './server'
+const args:Array<string> = process.argv.splice(2)
+const app = new ModifiedKoa(__dirname, process.env.NODE_ENV )
 
-app()
+const targetIndex:number = args.indexOf('--target')
+const target:string = args[targetIndex + 1] || ''
+
+if (target === 'admin' && !app.isProduction) {
+  app.runDevAdmin()
+} else if (!app.isProduction) {
+  app.runDev()
+} else {
+  app.runProduction()
+}
+app.start()
--- server.ts
+++ server.ts
@@ -11,65 +11,90 @@ const { Nuxt, Builder } = require('nuxt-edge')
 // Import and Set Nuxt.js options
 let nuxtConfig = require('./nuxt.config.js')
 nuxtConfig.dev = !(process.env.NODE_ENV === 'production')
-const args:Array<String> = process.argv.splice(2)
 
 
+class ModifiedKoa extends Koa {
+  readonly BaseDir: string
+  readonly isProduction: boolean
+  Sequelize: any
+  model: any
+  config: any
 
-async function start () {
-  console.log(`Running on ${process.env.NODE_ENV || 'development'} mode`)
-  let app = new Koa()
-  app.BaseDir = __dirname
-  app.isProduction = (process.env.NODE_ENV === 'production')
-  process.env.BASE_URL = config.baseUrl
-  // load config
-  config(app, app.BaseDir)
-  // set cookie key
-  app.keys = app.config.cookieKey
-  // logger
-  app.use(logger())
-  // error handler
-  app.use(errorHandler(app))
-  // bodyparser
-  app.use(koaBody({ multipart: true }))
-  // connect database
-  sequlize(app, app.config.db)
-  const ADMIN_DIST_DIR = path.join(__dirname, app.config.AdminDir.dist)
-  const STATIC_DIR = path.join(__dirname, app.config.AdminDir.static)
-  app.use(staticMiddleware({
-    dir: [ADMIN_DIST_DIR, STATIC_DIR],
-    prefix: '/admin'
-  }, app))
-  const nuxt = new Nuxt(nuxtConfig)
-  const targetIndex = args.indexOf('--target')
-  const target = args[targetIndex + 1] || ''
-  if (target === 'admin' && !app.isProduction) {
-    console.log('Dev Admin')
-    // 开发模式下使用 koa-webpack 热更新页面
-    app.use(router.routes())
-    const webpackMiddleware = require('./middlewares/webpackMiddleware')
-    webpackMiddleware(app)
-  } else if (!app.isProduction) {
-    // Build in development
-    const builder = new Builder(nuxt)
-    await builder.build()
-    app.use(router.routes())
-  } else {
+  constructor(BaseDir: string, NODE_ENV:string) {
+    console.log(`Running on ${NODE_ENV || 'development'} mode`)
+    super()
+    this.BaseDir = BaseDir
+    this.isProduction = (NODE_ENV === 'production')
+    process.env.BASE_URL = config.baseUrl
+    // load config
+    config(this, this.BaseDir)
+    // set cookie key
+    this.keys = this.config.cookieKey
+    // logger
+    this.use(logger())
+    // error handler
+    this.use(errorHandler(this))
+    // bodyparser
+    this.use(koaBody({ multipart: true }))
+    // connect database
+    sequlize(this, this.config.db)
+    const ADMIN_DIST_DIR:string = path.join(__dirname, this.config.AdminDir.dist)
+    const STATIC_DIR:string = path.join(__dirname, this.config.AdminDir.static)
+    this.use(staticMiddleware({
+      dir: [ADMIN_DIST_DIR, STATIC_DIR],
+      prefix: '/admin'
+    }, this))
+  }
+
+  async runProduction():Promise<any> {
+    const nuxt = new Nuxt(nuxtConfig)
     // add router middleware:
-    app.use(router.routes())
-    app.use(async (ctx: Koa.Context, next) => {
+    this.use(router.routes())
+    this.use(async (ctx: Koa.Context, next) => {
       await next()
       ctx.status = 200 // koa defaults to 404 when it sees that status is unset
       return new Promise((resolve, reject) => {
         ctx.res.on('close', resolve)
         ctx.res.on('finish', resolve)
-        nuxt.render(ctx.req, ctx.res, promise => {
+        nuxt.render(ctx.req, ctx.res, (promise: any) => {
           // nuxt.render passes a rejected promise into callback on error.
           promise.then(resolve).catch(reject)
         })
       })
     })
   }
-  app.listen(app.config.port, () => console.log(`app listening on port ${app.config.port}!`))
+
+  runDevAdmin():void {
+    console.log('Dev Admin')
+    // 开发模式下使用 koa-webpack 热更新页面
+    this.use(router.routes())
+    const webpackMiddleware = require('./middlewares/webpackMiddleware')
+    webpackMiddleware(this)
+  }
+
+  async runDev():Promise<any> {
+    const nuxt = new Nuxt(nuxtConfig)
+    // Build in development
+    const builder = new Builder(nuxt)
+    await builder.build()
+    this.use(router.routes())
+    this.use(async (ctx: Koa.Context, next) => {
+      await next()
+      ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+      return new Promise((resolve, reject) => {
+        ctx.res.on('close', resolve)
+        ctx.res.on('finish', resolve)
+        nuxt.render(ctx.req, ctx.res, (promise: any) => {
+          // nuxt.render passes a rejected promise into callback on error.
+          promise.then(resolve).catch(reject)
+        })
+      })
+    })
+  }
+
+  start() {
+    this.listen(this.config.port, () => console.log(`app listening on port ${this.config.port}!`))
+  }
 }
 
-module.exports = start
+export default ModifiedKoa
