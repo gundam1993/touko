b9e0250a56d904ad694b51afb93a74d43153d5fb
gundam1993 <gundam1993@gmail.com>
2018-06-29 15:18:28 +0800

    rewrite login check middleware with typescript

---
+++ middlewares/loginCheck.ts
@@ -0,0 +1,20 @@
+/// <reference path="../modifiedKoa.d.ts" />
+import * as jwt from 'jsonwebtoken'
+import * as ModifiedKoa from '../server'
+import * as Koa from 'koa'
+
+var loginCheck:Koa.Middleware = async(ctx:ModifiedKoa.ModifiedContext, next) => {
+  let token:string = ctx.cookies.get('touko-blog-token') || ctx.request.headers['x-token']
+  console.log(token)
+  try {
+    let user = jwt.verify(token, ctx.app.config.jwt.key)
+    ctx.userInfo = user
+    await next()
+  } catch (err) {
+    console.log(err)
+    ctx.status = 200
+    ctx.response.body = {success: 0, msg: err}
+  }
+}
+
+module.exports = loginCheck
