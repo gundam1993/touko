d13e3414c4e02338f26c8586be4a152990a6d254
gundam1993 <gundam1993@gmail.com>
2018-07-04 10:46:03 +0800

    bug fix

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcryptjs'
 import * as jwt from 'jsonwebtoken'
 import * as Koa from 'koa'
 import { ModifiedContext } from '../typings/app';
