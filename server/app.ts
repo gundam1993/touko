e08525015b6b1270b9fd4233e163e1a0615e0d29
gundam1993 <gundam1993@gmail.com>
2018-08-14 14:44:40 +0800

    update

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,9 @@
 import ModifiedKoa from './server'
 const app = new ModifiedKoa(__dirname, process.env.NODE_ENV )
 
-if (!app.isProduction) {
-  app.runDev()
-} else {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
   app.runProduction()
+} else {
+  app.runDev()
 }
 app.start()
