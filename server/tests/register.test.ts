de92c175f88d09d5d217e94140fe791e8efd6452
gundam1993 <gundam1993@gmail.com>
2018-08-14 17:17:00 +0800

    update test

--- server/tests/register.test.ts
+++ server/tests/register.test.ts
@@ -1,4 +1,12 @@
-import {request} from 'graphql-request'
+import { request } from 'graphql-request'
+import { User } from '../entities/user';
+import ModifiedKoa from '../server'
+const app = new ModifiedKoa(__dirname, process.env.NODE_ENV )
+
+beforeAll(async () => {
+  await app.runProduction()
+  await app.start()
+})
 
 const username = 'testUsername'
 const password = 'kabgfasdjk'
@@ -11,6 +19,10 @@ mutation {
 }
 `
 test('Register user', async () => {
-  const response = await request('http://localhost:3001/graphql', mutation)
+  const response = await request(`http://localhost:${app.config.port}/graphql`, mutation)
   expect(response).toEqual({"register": {"username": username}})
+  const users = await User.find({ where: {username}})
+  expect(users).toHaveLength(1);
+  const user = users[0]
+  expect(user.password).not.toEqual(password)
 })
