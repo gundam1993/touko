f0e56866b8ca2e7c73e206f2f5103b90cd180890
gundam1993 <gundam1993@gmail.com>
2018-08-17 11:37:13 +0800

    add login test

---
+++ server/tests/login.test.ts
@@ -0,0 +1,48 @@
+import { request } from "graphql-request"
+import { User } from '../entities/user'
+import { createConnection } from "typeorm"
+import { invalidLogin } from "../graphql/login/errorMessages";
+const config:any = require('../config/test')
+
+const username = 'testUsername'
+const password = '123456'
+const host = `${process.env.TEST_HOST as String}/graphql`
+
+const registerMutation = (u: string, p: string) => `
+mutation {
+  register(username: "${u}", password: "${p}") {
+    path
+    message
+  }
+}
+`
+
+const loginMutation = (u: string, p: string) => `
+mutation {
+  login(username: "${u}", password: "${p}") {
+    path
+    message
+  }
+}
+`
+
+const badLogin = async (u:string, p:string) => {
+  const response = await request(host, loginMutation(u, p))
+    expect(response).toEqual({
+      login: [{
+        path: 'username',
+        message: invalidLogin
+      }]
+    })
+}
+
+describe('login', () => {
+  test('bad username', async () => {
+    await badLogin("aaasssddd", "123456")
+  })
+
+  test('bad password', async () => {
+    await request(host, registerMutation("aaasssddd", "123456"))
+    await badLogin("aaasssddd", "654321")
+  })
+}) 
